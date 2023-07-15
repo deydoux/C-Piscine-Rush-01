0ddfc943340a8603134a023cd41e4aea55a45164
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-15 13:52:43 +0200

    Fix put_error use

--- ex00/rush-01.c
+++ ex00/rush-01.c
@@ -6,11 +6,11 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 09:10:54 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/15 10:49:50 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/15 13:52:12 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	throw_error(int	code);
+int	put_error(int code);
 int	**parse_params(char *input);
 
 int	main(int argc, char **argv)
@@ -18,8 +18,8 @@ int	main(int argc, char **argv)
 	int	**params;
 
 	if (argc != 2)
-		return (throw_error(1));
+		return (put_error(2));
 	params = parse_params(argv[1]);
 	if (!params)
-		return (throw_error(1));
+		return (put_error(2));
 }
