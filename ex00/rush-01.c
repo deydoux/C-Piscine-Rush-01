f11176c64c00c2b1bf0f705d9f76fa41f71e7743
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-15 11:07:48 +0200

    Add parse_params

--- ex00/rush-01.c
+++ ex00/rush-01.c
@@ -6,15 +6,20 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 09:10:54 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/15 09:41:16 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/15 10:49:50 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	throw_error(int	code);
+int	**parse_params(char *input);
 
 int	main(int argc, char **argv)
 {
+	int	**params;
+
 	if (argc != 2)
 		return (throw_error(1));
-	(void)argv;
+	params = parse_params(argv[1]);
+	if (!params)
+		return (throw_error(1));
 }
